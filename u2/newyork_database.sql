ee60b6bc7a2fd2835718339842578607a4de1e30
autarch princeps <autarch@outlook.com>
2015-04-24 13:59:55 +0200

    u2 Import von Moodle
    Ich habe noch nichts daran geändert

---
+++ u2/newyork_database.sql
@@ -0,0 +1,44 @@
+-- ***************************************************************
+-- * File Name:                  newyork_database.sql            *
+-- * File Creator:               Knolle                          *
+-- * CreationDate:               12. April 2015                  *
+-- *                                                             *
+-- * <ChangeLogDate>             <ChangeLogText>                 *
+-- ***************************************************************
+--
+-- ***************************************************************
+-- * Verteilte Datenbanksysteme SS 2015
+-- * Übung 2 
+-- * Allokation
+--
+-- ***************************************************************
+-- * SQL*plus Job Control Section
+--
+set 	echo 		on
+set 	linesize 	256
+set 	pagesize 	50
+set     escape      \
+spool   ./newyork_database.log
+
+--
+-- Systemdatum Start
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+-- ***************************************************************
+-- * Das lokale Schema für den Standort New York
+--
+
+
+--
+-- Systemdatum Ende
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+spool off
