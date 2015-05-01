8f6c0a043d49cb6ee85e9531dd31e292b007c205
autarch princeps <autarch@outlook.com>
2015-05-01 18:59:26 +0200

    u2 create tables & inserts

--- u2/bonn_database.sql
+++ u2/bonn_database.sql
@@ -28,10 +28,117 @@ spool   ./bonn_database.log
   FROM   dual
   ;  
 --
+drop table bnn_bnn_article purge;
+drop table bnn_customer purge;
+drop table depot purge;
+drop table supplier purge;
+drop table bnn_rent purge;
+
 -- ***************************************************************
 -- * Das lokale Schema f¸r den Standort Bonn  
 --
 
+create table bnn_article (
+  ID_article numeric(8) not null,
+  item varchar(128) not null,
+  type varchar(64) not null,
+  ID_supplier numeric(8) not null,
+  pur_basePrice numeric(8,2) not null,
+  pur_currency varchar(16) not null,
+  pur_purchasedate date not null,
+  sal_rentalPriceMonth numeric(8,2) not null,
+  sal_rentalPriceWeek numeric(8,2) not null,
+  sal_rentalPriceDay numeric(8,2) not null,
+  sal_currency varchar(16) not null,
+  ID_depot numeric(8),
+  dep_location numeric(6) not null,
+  dep_weightKg numeric(6,2) not null,
+  dep_heightCm numeric(6,2) not null,
+  dep_lengthCm numeric(6,2) not null,
+  dep_breadthCm numeric(6,2) not null,
+  constraint PK_article primary key (ID_article),
+  constraint UK_article unique (ID_depot, dep_location)
+);
+
+create table bnn_customer (
+  ID_customer numeric(8) not null,
+  company varchar(128) not null,
+  address varchar(32) not null,
+  zip varchar(16) not null,
+  city varchar(32) not null,
+  state varchar(32) not null,
+  constraint PK_customer primary key (ID_customer)
+);
+
+create table depot (
+  ID_depot numeric(8) not null,
+  company varchar(32) not null,
+  address varchar(32) not null,
+  zip varchar(16) not null,
+  city varchar(32) not null,
+  state varchar(32) not null,
+  constraint PK_depot primary key (ID_depot)
+);
+
+create table supplier (
+  ID_supplier numeric(8) not null,
+  company varchar(128) not null,
+  address varchar(32) not null,
+  zip varchar(16) not null,
+  city varchar(32) not null,
+  state varchar(32) not null,
+  constraint PK_supplier primary key (ID_supplier)
+);
+
+create table bnn_rent (
+  ID_customer numeric(8) not null,
+  ID_article numeric(8) not null,
+  contract numeric(8) not null,
+  rentFrom date not null,
+  rentTo date not null,
+  returnFlag numeric(1) not null,
+  constraint PK_rent primary key (ID_article, ID_customer, contract)
+);
+
+alter table bnn_article add constraint RK_bnn_article_depot
+  foreign key (ID_depot)
+  references depot;
+alter table bnn_article add constraint RK_bnn_article_supplier
+  foreign key (ID_supplier)
+  references supplier;
+alter table bnn_rent add constraint RK_bnn_rent_customer
+  foreign key (ID_customer)
+  references bnn_customer;
+
+-- Inserts
+-- table depot
+Insert into depot values (1, 'Baumaschinen Verleih Bonn', 'Buddestraﬂe 30', '53111', 'Bonn', 'Germany');
+Insert into depot values (2, 'The Hireman values (London) Ltd.', '53 Tanner Street', 'SE1 3PL', 'London', 'United Kingdom');
+Insert into depot values (3, 'ABLE Equipment Rental', '21 Dixon Avenue', 'NY 11726', 'Copiague', 'USA');
+-- table supplier
+Insert into supplier values (1, 'S¸ddeutsche Baumaschinen Handels GmbH', 'Heinkelstraﬂe 8', 'Neu-Ulm', '89231', 'Germany');
+Insert into supplier values (2, 'Maschinenhandel \& Vermietung', 'Weseler Straﬂe 68', 'Alpen', '46519', 'Germany');
+Insert into supplier values (3, 'ATLAS Mortag', 'Auf dem Waidrasen 10', 'Bonn', '53111', 'Germany');
+-- table bnn_article
+Insert into bnn_article values (17, 'New-Holland', 'Excavator', 3, 18000.00, 'Euro', to_date('2010-06-07', 'yyyy-mm-dd'), 874.00, 340.00, 85.00, 'Euro', 1, 53, 1600.00, 250.00, 300.00, 100.00);
+Insert into bnn_article values (18, 'Yanmar, ViO30', 'Excavator', 2, 25000.00, 'Euro', to_date('2010-09-10', 'yyyy-mm-dd'), 2500.00, 900.00, 190.40, 'Euro', 1, 12, 3270.00, 300.00, 400.00, 150.00);
+-- table bnn_customer
+Insert into bnn_customer values (11, 'BB Hoch- und Tiefbau GmbH', 'Tettaustraﬂe 1', 'Bonn', '53111', 'Germany');
+Insert into bnn_customer values (12, 'Canzler Bau GmbH', 'Daasdorfer Straﬂe 38', 'Weimar', '99428', 'Germany');
+Insert into bnn_customer values (13, 'Etzoldt Bau GmbH Hoch- und Tiefbau', 'Mittelstraﬂe 23', 'Jena', '07745', 'Germany');
+Insert into bnn_customer values (14, 'Kkd Tiefbau Weimar GmbH', 'Dammstr. 2', 'Apolda', '99510', 'Germany');
+Insert into bnn_customer values (15, 'TBI Holdings B.V.', 'Wilhelminaplein 37', 'Rotterdam', '3072 DE', 'Netherlands');
+-- table bnn_rent
+Insert into bnn_rent values (11, 17, 1, to_date('2015-05-10', 'yyyy-mm-dd'), to_date('2015-05-16', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (11, 17, 2, to_date('2015-07-20', 'yyyy-mm-dd'), to_date('2015-08-19', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (12, 18, 1, to_date('2015-03-11', 'yyyy-mm-dd'), to_date('2015-04-10', 'yyyy-mm-dd'), 1);
+Insert into bnn_rent values (12, 18, 2, to_date('2015-05-22', 'yyyy-mm-dd'), to_date('2015-05-28', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (13, 17, 1, to_date('2015-12-27', 'yyyy-mm-dd'), to_date('2015-12-31', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (13, 18, 1, to_date('2015-06-17', 'yyyy-mm-dd'), to_date('2015-06-21', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (14, 17, 1, to_date('2015-05-18', 'yyyy-mm-dd'), to_date('2015-05-24', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (14, 18, 1, to_date('2016-01-10', 'yyyy-mm-dd'), to_date('2016-02-09', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (15, 17, 1, to_date('2015-06-10', 'yyyy-mm-dd'), to_date('2015-06-12', 'yyyy-mm-dd'), 0);
+Insert into bnn_rent values (15, 17, 2, to_date('2016-02-05', 'yyyy-mm-dd'), to_date('2016-02-10', 'yyyy-mm-dd'), 0);
 
 --
 -- Systemdatum Ende
--- u2/newyork_database.sql
+++ u2/newyork_database.sql
@@ -31,7 +31,75 @@ spool   ./newyork_database.log
 -- ***************************************************************
 -- * Das lokale Schema für den Standort New York
 --
+create table nyk_article (
+  ID_article numeric(8) not null,
+  item varchar(128) not null,
+  type varchar(64) not null,
+  ID_supplier numeric(8) not null,
+  pur_basePrice numeric(8,2) not null,
+  pur_currency varchar(16) not null,
+  pur_purchasedate date not null,
+  sal_rentalPriceMonth numeric(8,2) not null,
+  sal_rentalPriceWeek numeric(8,2) not null,
+  sal_rentalPriceDay numeric(8,2) not null,
+  sal_currency varchar(16) not null,
+  ID_depot numeric(8),
+  dep_location numeric(6) not null,
+  dep_weightKg numeric(6,2) not null,
+  dep_heightCm numeric(6,2) not null,
+  dep_lengthCm numeric(6,2) not null,
+  dep_breadthCm numeric(6,2) not null,
+  constraint PK_article primary key (ID_article),
+  constraint UK_article unique (ID_depot, dep_location)
+);
 
+create table nyk_customer (
+  ID_customer numeric(8) not null,
+  company varchar(128) not null,
+  address varchar(32) not null,
+  zip varchar(16) not null,
+  city varchar(32) not null,
+  state varchar(32) not null,
+  constraint PK_customer primary key (ID_customer)
+);
+
+create table nyk_rent (
+  ID_customer numeric(8) not null,
+  ID_article numeric(8) not null,
+  contract numeric(8) not null,
+  rentFrom date not null,
+  rentTo date not null,
+  returnFlag numeric(1) not null,
+  constraint PK_rent primary key (ID_article, ID_customer, contract)
+);
+
+alter table nyk_rent add constraint RK_nyk_rent_customer
+  foreign key (ID_customer)
+  references nyk_customer;
+
+-- Inserts
+-- table nyk_article
+Insert into nyk_article values (31, 'Bomag Vibratory Roller - Ride-On - Smooth/Padfoot 48" Drum Width, BW124-3, 6,000 lbs', 'Vibratory Roller', 1, 20000.00, 'Euro', to_date('2010-10-23', 'yyyy-mm-dd'), 1004.00, 251.00, 104.00, 'Dollar', 3, 38, 2721.00, 120.00, 150.00, 200.00);
+Insert into nyk_article values (32, 'Bomag Vibratory Roller - Ride-On - Smooth/Padfoot Asphalt Roller - 3 Ton Vibratory Bomag BW120', 'Vibratory Roller', 1, 24000.00, 'Euro', to_date('2010-04-12', 'yyyy-mm-dd'), 1200.00, 300.00, 150.00, 'Dollar', 3, 64, 3000.00, 180.00, 200.00, 250.00);
+Insert into nyk_article values (33, 'Industrial - 5000 lbs Cushion Triple Mast', 'Fork Lift', 2, 16000.00, 'Euro', to_date('2010-03-01', 'yyyy-mm-dd'), 1300.00, 495.00, 165.00, 'Dollar', 3, 12, 1800.00, 250.00, 250.00, 150.00);
+Insert into nyk_article values (34, 'Industrial - 8,000 lbs Cushion Triple Mast', 'Fork Lift', 2, 23000.00, 'Euro', to_date('2010-08-20', 'yyyy-mm-dd'), 2430.00, 810, 270, 'Dollar', 3, 13, 1900.00, 280.00, 250.00, 150.00);
+-- table nyk_customer
+Insert into nyk_customer values (310, 'Parsons Brinckerhoff', 'One Penn Plaza', 'New York', 'NY 10119', 'United States');
+Insert into nyk_customer values (311, 'Gannett Fleming', '380 Seventh Avenue', 'New York', 'NY 10121-0101', 'United States');
+Insert into nyk_customer values (312, 'Rhode Island Engineering', '397 Mourning Dove Drive', 'Saunderstown', 'RI', 'United States');
+Insert into nyk_customer values (313, 'American Engineering Co', '400 South County Trail', 'Exeter', 'RI', 'United States');
+Insert into nyk_customer values (314, 'EP and CC Consulting Inc', '24 Rosevear Avenue', 'Toronto', 'ON M4C 1Z3', 'Canada');
+-- table nyk_rent
+Insert into nyk_rent values (310, 31, 1, to_date('2015-03-10', 'yyyy-mm-dd'), to_date('2015-04-09', 'yyyy-mm-dd'), 1);
+Insert into nyk_rent values (310, 32, 1, to_date('2015-02-10', 'yyyy-mm-dd'), to_date('2015-02-15', 'yyyy-mm-dd'), 1);
+Insert into nyk_rent values (311, 33, 1, to_date('2015-07-11', 'yyyy-mm-dd'), to_date('2015-07-18', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (311, 34, 1, to_date('2015-06-22', 'yyyy-mm-dd'), to_date('2015-06-28', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (312, 31, 1, to_date('2015-05-06', 'yyyy-mm-dd'), to_date('2015-05-12', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (312, 32, 1, to_date('2015-03-22', 'yyyy-mm-dd'), to_date('2015-03-28', 'yyyy-mm-dd'), 1);
+Insert into nyk_rent values (313, 33, 1, to_date('2016-01-03', 'yyyy-mm-dd'), to_date('2016-01-10', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (313, 34, 1, to_date('2015-12-15', 'yyyy-mm-dd'), to_date('2015-12-21', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (314, 31, 1, to_date('2016-01-02', 'yyyy-mm-dd'), to_date('2016-02-03', 'yyyy-mm-dd'), 0);
+Insert into nyk_rent values (314, 32, 1, to_date('2015-09-17', 'yyyy-mm-dd'), to_date('2015-09-24', 'yyyy-mm-dd'), 0);
 
 --
 -- Systemdatum Ende
