9f25e0619ebbaad5c76681b5cd6900875b854f38
autarch princeps <autarch@outlook.com>
2015-05-05 13:43:03 +0200

    u3 init

---
+++ u3/bonn_global_database.sql
@@ -0,0 +1,72 @@
+-- ***************************************************************
+-- * File Name:                  bonn_global_database.sql        *
+-- * File Creator:               Knolle                          *
+-- * CreationDate:               04. May 2015                    *
+-- *                                                             *
+-- * <ChangeLogDate>             <ChangeLogText>                 *
+-- ***************************************************************
+--
+-- ***************************************************************
+-- * Verteilte Datenbanken SS 2015
+-- * Übung 3 
+-- * Globales Schema
+--
+-- ***************************************************************
+-- * SQL*plus Job Control Section
+--
+set 	echo 		on
+set 	linesize 	256
+set 	pagesize 	50
+set     escape      \
+spool   ./bonn_global_database.log
+
+--
+-- Systemdatum Start
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+-- ***************************************************************
+-- * Das globale Schema fuer den Standort Bonn  
+--
+
+--
+-- ***************************************************************
+-- * Initialisierung  
+--
+
+DROP SYNONYM ...
+
+DROP VIEW ...
+
+DROP DATABASE LINK ...
+
+--
+-- ***************************************************************
+-- * Erstellung  
+--
+
+CREATE DATABASE LINK ...
+
+CREATE VIEW ...
+
+CREATE SYNONYM ...
+
+--
+-- ***************************************************************
+-- * Test der zehn Anwendungsfaelle  
+--
+
+SELECT ...
+
+--
+-- Systemdatum Ende
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+spool off
