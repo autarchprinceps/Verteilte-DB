c7a5363eb351ed70016592b9fda90ef7197933a0
Alex <alex@smartpioneers.com>
2015-05-15 16:17:48 +0200

    fix typo ldn, next problem: IF Abfrage bei abhängiger Fragmentierung

--- u4/bonn_global_pk_database.sql
+++ u4/bonn_global_pk_database.sql
@@ -105,7 +105,7 @@ IF :new.state = 'Germany' OR :new.state = 'Nethderlands' THEN
 INSERT INTO BNN_CUSTOMER values (NULL,
 :new.COMPANY, :new.ADDRESS, :new.ZIP, :new.CITY, :new.STATE);
 ELSIF :new.state = 'United Kingdom' THEN
-INSERT INTO LND_CUSTOMER@london values (NULL,
+INSERT INTO LDN_CUSTOMER@london values (NULL,
 :new.COMPANY, :new.ADDRESS, :new.ZIP, :new.CITY, :new.STATE);
 ELSIF :new.state = 'USA' THEN
 INSERT INTO NYK_CUSTOMER@newyork values (NULL,
@@ -117,11 +117,53 @@ IF :old.state = 'Germany' OR :old.state = 'Nethderlands' THEN
 DELETE FROM BNN_CUSTOMER WHERE ID_CUSTOMER =
 :old.ID_CUSTOMER;
 elsif :old.state = 'United Kingdom' THEN
-DELETE FROM LND_CUSTOMER@london WHERE ID_CUSTOMER =
+DELETE FROM LDN_CUSTOMER@london WHERE ID_CUSTOMER =
 :old.ID_CUSTOMER;
 elsif :old.state = 'USA' THEN
 DELETE FROM NYK_CUSTOMER@newyork WHERE ID_CUSTOMER =
 :old.ID_CUSTOMER;
+END IF;
+END IF;
+END;
+/
+
+
+-- **VIEW ARTICLE -- Problem: abhängige Fragmentierung: wie referenziert man im If-Statement einen Wert aus der DEPOT Tabelle?
+CREATE OR REPLACE TRIGGER ARTICLE_GLO_TRI
+INSTEAD OF INSERT OR DELETE ON article
+FOR EACH ROW
+BEGIN
+IF INSERTING THEN
+IF :new.state = 'Germany' OR :new.state = 'Nethderlands' THEN --TODO: ersetzen durch Abfrage d. abhängigen Tabelle DEPOT
+INSERT INTO BNN_ARTICLE values (NULL,
+:new.ITEM, :new.TYPE, :new.ID_SUPPLIER, :new.PUR_BASEPRICE, :new.PUR_CURRENCY, 
+:new.PUR_PURCHASEDATE, :new.SAL_RENTALPRICEMONTH, :new.SAL_RENTALPRICEWEEK, 
+:new.SAL_RENTALPRICEDAY,:new.SAL_CURRENCY, :new.ID_DEPOT, :new.DEP_LOCATION, 
+:new.DEP_WEIGHTKG, :new.DEP_HEIGHTCM, :new.DEP_LENGTHCM, :new.DEP_BREADTHCM);
+ELSIF :new.state = 'United Kingdom' THEN
+INSERT INTO LDN_ARTICLE@london values (NULL,
+:new.ITEM, :new.TYPE, :new.ID_SUPPLIER, :new.PUR_BASEPRICE, :new.PUR_CURRENCY, 
+:new.PUR_PURCHASEDATE, :new.SAL_RENTALPRICEMONTH, :new.SAL_RENTALPRICEWEEK, 
+:new.SAL_RENTALPRICEDAY, :new.SAL_CURRENCY, :new.ID_DEPOT, :new.DEP_LOCATION, 
+:new.DEP_WEIGHTKG, :new.DEP_HEIGHTCM, :new.DEP_LENGTHCM, :new.DEP_BREADTHCM);
+ELSIF :new.state = 'USA' THEN
+INSERT INTO NYK_ARTICLE@newyork values (NULL,
+:new.ITEM, :new.TYPE, :new.ID_SUPPLIER, :new.PUR_BASEPRICE, :new.PUR_CURRENCY, 
+:new.PUR_PURCHASEDATE, :new.SAL_RENTALPRICEMONTH, :new.SAL_RENTALPRICEWEEK, 
+:new.SAL_RENTALPRICEDAY, :new.SAL_CURRENCY, :new.ID_DEPOT, :new.DEP_LOCATION, 
+:new.DEP_WEIGHTKG, :new.DEP_HEIGHTCM, :new.DEP_LENGTHCM, :new.DEP_BREADTHCM);
+END IF;
+END IF;
+IF DELETING THEN
+IF :old.state = 'Germany' OR :old.state = 'Nethderlands' THEN --TODO: ersetzen durch Referenzierung auf DEPOT.state
+DELETE FROM BNN_ARTICLE WHERE ID_ARTICLE =
+:old.ID_ARTICLE;
+elsif :old.state = 'United Kingdom' THEN
+DELETE FROM LDN_ARTICLE@london WHERE ID_ARTICLE =
+:old.ID_ARTICLE;
+elsif :old.state = 'USA' THEN
+DELETE FROM NYK_ARTICLE@newyork WHERE ID_ARTICLE =
+:old.ID_ARTICLE;
 END IF;
 END IF;
 END;
