e0e2cc2bc3b1f7f4ebbe5aa397ea22f1897d578b
autarch princeps <autarch@outlook.com>
2015-06-02 11:40:02 +0200

    u6 init

---
+++ u6/test_replication_database.sql
@@ -0,0 +1,45 @@
+-- ***************************************************************
+-- * File Name:                  test_replication_database.sql   *
+-- * File Creator:               Knolle                          *
+-- * CreationDate:               23. Mai 2015                    *
+-- *                                                             *
+-- * <ChangeLogDate>             <ChangeLogText>                 *
+-- ***************************************************************
+--
+-- ***************************************************************
+-- * Verteilte Datenbanksysteme SS 2015
+-- * Uebung 6 
+-- * Replikation
+--
+-- ***************************************************************
+-- * SQL*plus Job Control Section
+--
+set 	echo 		on
+set 	linesize 	256
+set 	pagesize 	50
+set     escape      \
+spool   ./test_replication_database.log
+
+--
+-- Systemdatum Start
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+-- ***************************************************************
+-- * Globales Testskript  
+--
+
+
+
+--
+-- Systemdatum Ende
+--
+  SELECT user,
+         TO_CHAR(SYSDATE, 'dd-mm-yy hh24:mi:ss') 
+  FROM   dual
+  ;  
+--
+spool off
